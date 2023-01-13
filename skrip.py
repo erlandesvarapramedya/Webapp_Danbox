4e881910151a90736a4fb2b96af28e96eadbaaeb
DESKTOP-MMHF5F0\User <erlanpd@gmail.com>
2023-01-13 23:17:22 +0700

    initial deployment

---
+++ skrip.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.title("DANBOX")
+st.write("Ini adalah aplikasi untuk memesan makanan danus")
+
+jumlah=st.number_input("Jumlah")
+
+st.write(1234)
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
+
+st.write(Data_frame
